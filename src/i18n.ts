b150278ba1b6d6b0c659788c60f174029e941dec
Dipesh Mali <dipeshmali44@gmail.com>
2024-04-03 18:49:02 +0530

    Make change in host url

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,7 +2,7 @@ import i18n from "i18next";
 import i18nBackend from "i18next-http-backend";
 import { initReactI18next } from "react-i18next";
 
-const getCurrentHost = import.meta.env.MODE === 'development' ? 'http://localhost:3000' : ''
+const getCurrentHost = import.meta.env.MODE === 'development' ? 'http://localhost:3000' : 'https://dipesh-multi-lang-app.netlify.app/'
 
 i18n
     .use(i18nBackend) // Need when load translation from backend (Public Folder)
